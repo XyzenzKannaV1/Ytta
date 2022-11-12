19ddc86a6ee52687eabe304864f2eb3b37c29ee8
Kanade ClanZ <112399198+XyzenzKannaV1@users.noreply.github.com>
2022-11-12 18:10:26 +0700

    Update mods.js

--- commands/special/mods.js
+++ commands/special/mods.js
@@ -7,17 +7,17 @@ exports.run = {
       // if (command == 'owner') return client.reply(m.chat, `Interkasi owner dengan user via WhatsApp sudah tidak tersedia lagi, jika ingin upgrade premium atau ada hal penting lainnya silahkan kirim pesan melalui email dengan subjek *"MINT"*\n\n✉️ : contact@neoxr.my.id`, m)
       if (command == 'mods') {
          const contact = [{
-            name: 'Lord Senku',
-            number: '62882019091063',
-            about: 'Moderator Rikkabot'
+            name: 'Lord Siesta',
+            number: '6283112831554',
+            about: 'Siesta Bot'
          }, {
             name: 'Bot ( Dev )',
-            number: '6285696481452',
-            about: 'Moderator RikkaBot'
+            number: '6288980870067',
+            about: 'Moderator Siesta'
          }, {
             name: 'Kids',
             number: global.owner,
-            about: 'Moderator Rikka Bot'
+            about: 'Moderator Siesta Bot'
          }]
          const msg = await client.sendContact(m.chat, contact, m)
          await client.reply(m.chat, `Silahkan hubungi moderator jika nomor anda ter banned dan ter block oleh bot`, msg)
