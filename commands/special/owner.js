78be198d6ad6ac3dcbc3d9898499adad21d7d142
Kanade ClanZ <112399198+XyzenzKannaV1@users.noreply.github.com>
2022-11-12 18:11:42 +0700

    Update owner.js

--- commands/special/owner.js
+++ commands/special/owner.js
@@ -4,10 +4,10 @@ exports.run = {
       client,
       command
    }) => {
-      // if (command == 'owner') return client.reply(m.chat, `Interkasi owner dengan user via WhatsApp sudah tidak tersedia lagi, jika ingin upgrade premium atau ada hal penting lainnya silahkan kirim pesan melalui email dengan subjek *"MINT"*\n\n✉️ : contact@neoxr.my.id`, m)
+      // if (command == 'owner') return client.reply(m.chat, `Interkasi owner dengan user via WhatsApp sudah tidak tersedia lagi, jika ingin upgrade premium atau ada hal penting lainnya silahkan kirim pesan melalui email dengan subjek *"MINT"*\n\n✉️ : contact@ai.my.id`, m)
       if (command == 'owner') {
          const contact = [{
-            name: 'FaruqWangsaf',
+            name: 'ᴀɪ',
             number: global.owner,
             about: 'Owner Wangsaf'
          }]
