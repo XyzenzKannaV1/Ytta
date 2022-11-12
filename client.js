d301d703d4294719270a060f35433d2d35e48f43
Kanade ClanZ <112399198+XyzenzKannaV1@users.noreply.github.com>
2022-11-12 20:06:13 +0700

    Update client.js

--- client.js
+++ client.js
@@ -147,7 +147,7 @@ const connect = async () => {
       let meta = await (await client.groupMetadata(gc.id))
       let member = gc.participants[0]
       let textwel = Func.texted('bold', `Welcome Kak +tag👋 Jangan Lupa Semangat yah .`)
-      let textleft = Func.texted('bold', `+tag Keluar Karena Ingir Keluar`)
+      let textleft = Func.texted('bold', `+tag Keluar Karena Mw Nenen Sama Tante Ai Aharen😋`)
       let readmore = String.fromCharCode(8206).repeat(4001)
       let deskripsi = await (await client.groupMetadata(gc.id)).desc.toString()
       let groupSet = global.db.groups[gc.id]
