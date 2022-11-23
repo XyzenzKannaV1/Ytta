53e30a6986ee69a48a70497a345c7ad79316f839
Kanade ClanZ <112399198+XyzenzKannaV1@users.noreply.github.com>
2022-11-23 16:07:51 +0700

    Update owner.js

--- commands/special/owner.js
+++ commands/special/owner.js
@@ -7,9 +7,9 @@ exports.run = {
       // if (command == 'owner') return client.reply(m.chat, `Interkasi owner dengan user via WhatsApp sudah tidak tersedia lagi, jika ingin upgrade premium atau ada hal penting lainnya silahkan kirim pesan melalui email dengan subjek *"MINT"*\n\n✉️ : contact@ai.my.id`, m)
       if (command == 'owner') {
          const contact = [{
-            name: 'ᴀɪ ᴀʜᴀʀᴇɴ',
+            name: 'Dina Basreng',
             number: global.owner,
-            about: 'Owner Wangsaf'
+            about: 'Owner Siesta Bot'
          }]
          const msg = await client.sendContact(m.chat, contact, m)
          await client.reply(m.chat, `Harap chat owner jika penting saja dan to the point, Misal untuk melakukan transaksi pembayaran premium, sewabot, unbanned dan unblock`, msg)
